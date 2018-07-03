c28ffc12db423dcd5cfad3e77117e701432115e5
chiwawaweb <git@chiwawaweb.com>
2018-07-03 13:32:23 +0200

    Maj

--- Recettes/Forms/MainForm.Designer.cs
+++ Recettes/Forms/MainForm.Designer.cs
@@ -28,13 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BtnNouvelleRecette = new System.Windows.Forms.Button();
+            this.BtnQuitter = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // BtnNouvelleRecette
+            // 
+            this.BtnNouvelleRecette.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnNouvelleRecette.Location = new System.Drawing.Point(287, 41);
+            this.BtnNouvelleRecette.Name = "BtnNouvelleRecette";
+            this.BtnNouvelleRecette.Size = new System.Drawing.Size(204, 102);
+            this.BtnNouvelleRecette.TabIndex = 0;
+            this.BtnNouvelleRecette.Text = "Nouvelle recette";
+            this.BtnNouvelleRecette.UseVisualStyleBackColor = true;
+            // 
+            // BtnQuitter
+            // 
+            this.BtnQuitter.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnQuitter.Location = new System.Drawing.Point(287, 389);
+            this.BtnQuitter.Name = "BtnQuitter";
+            this.BtnQuitter.Size = new System.Drawing.Size(204, 45);
+            this.BtnQuitter.TabIndex = 1;
+            this.BtnQuitter.Text = "Quitter";
+            this.BtnQuitter.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1057, 623);
+            this.ClientSize = new System.Drawing.Size(787, 468);
+            this.Controls.Add(this.BtnQuitter);
+            this.Controls.Add(this.BtnNouvelleRecette);
             this.Name = "MainForm";
             this.Text = "Recettes";
             this.ResumeLayout(false);
@@ -42,5 +66,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtnNouvelleRecette;
+        private System.Windows.Forms.Button BtnQuitter;
     }
 }
